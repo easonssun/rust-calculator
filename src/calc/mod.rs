31539efaa7b05df6c5560007de948c90e9bca07e
Eason Sun <ballentin133@qq.com>
2025-09-08 22:26:27 +0800

    feat: 完成 rust 计算器

--- src/calc/mod.rs
+++ src/calc/mod.rs
@@ -1,5 +1,31 @@
-mod token;
-mod tokenizer;
+use rust_decimal::Decimal;
+
+use crate::calc::{error::CalcResult, parser::Parser};
+
 mod ast;
 mod error;
 mod parser;
+mod token;
+mod tokenizer;
+
+pub fn calc(expression: &str) -> CalcResult<Decimal> {
+    let mut parser = Parser::new(expression)?;
+    let ast = parser.parse()?;
+    Ok(ast.eval())
+}
+
+#[cfg(test)]
+mod tests {
+    use rust_decimal::dec;
+
+    use super::*;
+
+    #[test]
+    fn test_calc() {
+        assert_eq!(calc("1+2").unwrap(), dec!(3));
+        assert_eq!(calc("1+2*3").unwrap(), dec!(7));
+        assert_eq!(calc("1+2*3/4").unwrap(), dec!(2.5));
+        assert_eq!(calc("-1^2").unwrap(), dec!(1));
+        assert_eq!(calc("3- (2+3) * 2 -1 *(-3*3)").unwrap(), dec!(2));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
-mod calc;
-fn main() {
+use std::io::Write;
 
+mod calc;
+
+fn main() {
+    println!("欢迎使用计算器!");
+    println!("输入算术表达式, 按回车开始计算, 支持 + - * / () ^ 符号。");
+    println!("输入退出请输入q。");
+
+    loop {
+        let mut input = String::new();
+        // rust 行缓冲，需要刷新才能显示在输出通一行
+        print!(">>> ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        if input.trim() == "q" {
+            println!("Bye!");
+            break;
+        } else if input.trim() == "" {
+            continue;
+        }
+        match calc::calc(&input) {
+            Ok(result) => println!("Result: {}", result),
+            Err(err) => println!("Err: {}", err),
+        }
+    }
 }
